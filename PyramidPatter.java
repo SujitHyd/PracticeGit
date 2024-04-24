e4318b89f923edc5e59cb09213586a25a02647f1
Sujit <154961038+SujitHyd@users.noreply.github.com>
2024-04-24 17:19:46 +0530

    New File Added

---
+++ PyramidPatter.java
@@ -0,0 +1,30 @@
+
+public class PyramidPatter {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i < 10; i++) {
+			for (int j = 0; j < i; j++) {
+				
+				System.out.print("*");
+				System.out.print("\t");
+				
+			}
+			System.out.println();
+			
+		}
+		for (int i = 10; i > 0; i--) {
+			for (int j = 0; j<i; j++) {
+				
+				System.out.print("*");
+				System.out.print("\t");
+				
+			}
+			System.out.println();
+		
+
+	}
+
+}
+}
+
