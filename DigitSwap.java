02b2c72fd387aaaa997edfc1bece38086d40c767
Sujit <154961038+SujitHyd@users.noreply.github.com>
2024-04-26 16:21:33 +0530

    SwapTest

---
+++ DigitSwap.java
@@ -0,0 +1,20 @@
+
+public class DigitSwap {
+
+	public static void main(String[] args) {
+		int a=6,b=8;
+		
+		b=a+b; //14
+		
+		a=b-a; //8
+		
+		b=b-a;
+		
+		System.out.println("a -- "+ a );
+		System.out.println("b -- "+ b );
+		
+		
+
+	}
+
+}
